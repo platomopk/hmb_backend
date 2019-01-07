b88226ab5c5b4b7d06c3408c2b4a34505df8b825
Khyzar Kapoor <khyzar@protonmail.com>
2019-01-07 19:44:50 +0500

    after cdn update12

--- routes/users.js
+++ routes/users.js
@@ -435,6 +435,7 @@ router.post('/updatechild',(req,res)=>{
                 'childs.$.schoolcity':req.body.schoolcity,
                 'childs.$.schoolname':req.body.schoolname,
                 'childs.$.grade':req.body.grade,
+                'childs.$.rollnumber':req.body.rollnumber,
                 'childs.$.section':req.body.section
             },
             (err,resp)=>{
